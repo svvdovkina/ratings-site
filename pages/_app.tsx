1f6d0533fc1631830fc774eb11c0187071de3a70
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-07-19 14:13:19 +0300

    set global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
+import Head from 'next/head'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Noto_Sans } from 'next/font/google'
+
+const noto_sans = Noto_Sans({
+  weight: ['300', '400', '500','700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Ratings</title>
+      <meta name="description" content="Site with ratings" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className={noto_sans.className}>
+      <Component {...pageProps} />
+    </main>
+   
+  </>
 }
