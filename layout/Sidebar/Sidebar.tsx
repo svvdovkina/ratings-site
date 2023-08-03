69631501feafdb6943d95145160a8584906dddb7
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-08-03 12:42:19 +0300

    finish sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,11 +2,13 @@ import { SidebarProps } from "./Sidebar.props"
 import styles from "./Sidebar.module.css"
 import classnames from 'classnames'
 import { Menu } from "../Menu/Menu"
+import Logo from "../logo.svg"
 
 export const Sidebar = ({className, ...props }: SidebarProps) 
 : JSX.Element =>{
     return <div {...props} className={classnames(className, styles.sidebar)}>
-        Sidebar:
+        <Logo className={styles.logo}/>
+        <div>Search...</div>
         <Menu/>
     </div>
 }
