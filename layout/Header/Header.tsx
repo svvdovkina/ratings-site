94768eb780907739f4889799fb23d15c8d9b666d
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-08-10 16:13:03 +0300

    add header component (for small screens)

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,10 +1,35 @@
 import { HeaderProps } from "./Header.props"
 import styles from "./Header.module.css"
 import classnames from 'classnames'
+import Logo from "../logo.svg"
+import MenuIcon from "./menu.svg"
+import { Button } from "@/components"
+import { Sidebar } from "../Sidebar/Sidebar"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 
 export const Header = ({className, ...props }: HeaderProps) 
 : JSX.Element =>{
-    return <div {...props} className={classnames(className, styles.header)}>
-        Header
-    </div>
+
+    const router = useRouter();
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    useEffect(()=>{
+        setIsMenuOpen(false);
+    }, [router])
+
+    const openMenu = ()=>setIsMenuOpen(true)
+    const closeMenu = ()=>setIsMenuOpen(false)
+
+    return <header {...props} className={classnames(className, styles.header)}>
+        <Logo className={styles.logo}/>
+        <Button appearance='ghost'  className={styles.menuBtn} onClick={openMenu}><MenuIcon/></Button>
+        {isMenuOpen && <div className={styles.mobileMenu}>
+            <Sidebar/>
+            <Button appearance='primary' className={styles.closeBtn} onClick={closeMenu}>&#9587;</Button>
+        </div>}
+    </header>
+    
+    
 }
